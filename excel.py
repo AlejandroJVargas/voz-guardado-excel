2aace34c58c801e257a5f6326ebb096c83c08688
AlejandroJVargas <alejandrojvargas.1998@gmail.com>
2024-10-28 21:12:13 -0300

    Se crea el archivo excel

--- excel.py
+++ excel.py
@@ -1,3 +1,10 @@
-import openpyxl
+from openpyxl import Workbook
 
-wb = openpyxl.Workbook()
+wb = Workbook()
+
+ws = wb.active
+ws.title = "Programa"
+
+wb.save("excel.xlsx")
+
+print("Tu archivo excel fue creado con exito")
